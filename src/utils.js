f77aa0d01a73ea15f1d9c5fc70169933550b4190
Ian Seabock <ianseabock@gmail.com>
2020-11-23 12:59:20 -0800

    key works; api responds with response object

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,22 @@
-const fetchWeather = cityName => {
-  const API_KEY = "<INSERT_API_KEY>";
-  // TODO: fetch weather forecast from endpoint
-  // from https://openweathermap.org/api
+const fetchWeather = async (cityName) => {
+    const API_KEY = process.env.REACT_APP_OWM_API_KEY;
+
+    try {
+        const response = await fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`,
+            {
+                method: 'GET',
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Request Error: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.log('error', error);
+    }
 };
+
+export default fetchWeather;

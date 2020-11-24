083b2d081b8b3a4bcc6a5771217f834943076634
Ian Seabock <ianseabock@gmail.com>
2020-11-24 11:36:03 -0800

    Functioning weather search

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,13 +7,20 @@ import {
     IconButton,
     Paper,
     Switch,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
     TextField,
+    TableRow,
     Typography,
     fade,
     makeStyles,
     withStyles,
 } from '@material-ui/core';
-import useFetchWeather from '../utils';
+import * as dayjs from 'dayjs';
+import useWeatherAndForcast from '../utils';
 
 import '../App.css';
 
@@ -23,7 +30,7 @@ function App() {
     const [unitsChecked, setUnitsChecked] = useState(true);
     const [units, setUnits] = useState('imperial');
 
-    const { isLoading, data, error } = useFetchWeather(cityName, units);
+    const { isLoading, data, error } = useWeatherAndForcast(cityName, units);
 
     const classes = useStyles();
 
@@ -114,18 +121,142 @@ function App() {
                         </Grid>
                     </form>
                 </FormGroup>
-                {console.log('data', data)}
                 {!isLoading && !error && (
-                    <Paper className={classes.weatherForcast}>
-                        <h3>{data.name}</h3>
-                        <ul>
-                            <li>Temp: {data.main.temp}</li>
-                            <li>Feels Like: {data.main.feels_like}</li>
-                            <li>Humidity: {data.main.humidity}</li>
-                            <li>High: {data.main.temp_max}</li>
-                            <li>Low: {data.main.temp_min}</li>
-                        </ul>
-                    </Paper>
+                    <>
+                        <h3 className={classes.sectionHeader}>
+                            Right Now in {data.weather.name}
+                        </h3>
+                        <TableContainer component={Paper}>
+                            <Table
+                                className={classes.table}
+                                size="small"
+                                aria-label="a dense table"
+                            >
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell
+                                            className={classes.tableHeader}
+                                            align="left"
+                                        >
+                                            Temp
+                                        </TableCell>
+                                        <TableCell
+                                            className={classes.tableHeader}
+                                            align="left"
+                                        >
+                                            Feels Like
+                                        </TableCell>
+                                        <TableCell
+                                            className={classes.tableHeader}
+                                            align="left"
+                                        >
+                                            High of
+                                        </TableCell>
+                                        <TableCell
+                                            className={classes.tableHeader}
+                                            align="left"
+                                        >
+                                            Low of
+                                        </TableCell>
+                                        <TableCell
+                                            className={classes.tableHeader}
+                                            align="left"
+                                        >
+                                            Humidity
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell align="left">
+                                            {data.weather.main.temp}
+                                            <span>&#176;</span>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            {data.weather.main.feels_like}
+                                            <span>&#176;</span>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            {data.weather.main.temp_max}
+                                            <span>&#176;</span>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            {data.weather.main.temp_min}
+                                            <span>&#176;</span>
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            {data.weather.main.humidity}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <h3 className={classes.sectionHeader}>
+                            Five Day Forcast
+                        </h3>
+                        <TableContainer component={Paper}>
+                            <Table
+                                className={classes.table}
+                                size="small"
+                                aria-label="a dense table"
+                            >
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell
+                                            align="left"
+                                            className={classes.tableHeader}
+                                        >
+                                            Day
+                                        </TableCell>
+                                        <TableCell
+                                            align="left"
+                                            className={classes.tableHeader}
+                                        >
+                                            Low
+                                        </TableCell>
+                                        <TableCell
+                                            align="left"
+                                            className={classes.tableHeader}
+                                        >
+                                            High
+                                        </TableCell>
+                                        <TableCell
+                                            align="left"
+                                            className={classes.tableHeader}
+                                        >
+                                            Humidity
+                                        </TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {data.forcast.list.map((row) => (
+                                        <TableRow key={row.dt}>
+                                            <TableCell
+                                                component="th"
+                                                scope="row"
+                                                align="left"
+                                            >
+                                                {dayjs
+                                                    .unix(row.dt)
+                                                    .format('dddd')}
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {row.temp.min}
+                                                <span>&#176;</span>
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {row.temp.max}
+                                                <span>&#176;</span>
+                                            </TableCell>
+                                            <TableCell align="left">
+                                                {row.humidity}
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </>
                 )}
             </Grid>
         </Container>
@@ -177,13 +308,14 @@ const useStyles = makeStyles({
     searchInput: {
         background: '#FFFFFF',
     },
+    sectionHeader: { color: '#666666' },
+    tableHeader: { fontWeight: 'bold' },
     searchArrow: {
         margin: '3px 0 0 0',
     },
     weatherForcast: {
         margin: '10px 0 0 0',
         width: '100%',
-        background: fade('#FFFFFF', 0.5),
     },
 });
 

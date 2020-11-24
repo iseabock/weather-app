b56006e882333214beca465910819e6824885923
Ian Seabock <ianseabock@gmail.com>
2020-11-23 17:45:38 -0800

    added unit switch. Needs work

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import {
     Container,
+    FormGroup,
     Grid,
     Icon,
     IconButton,
     Paper,
+    Switch,
     TextField,
+    Typography,
     fade,
     makeStyles,
+    withStyles,
 } from '@material-ui/core';
 import useFetchWeather from '../utils';
 
@@ -16,9 +20,17 @@ import '../App.css';
 function App() {
     const [cityInputValue, setCityInputValue] = useState('');
     const [cityName, setCityName] = useState('');
-    const { isLoading, data, error } = useFetchWeather(cityName);
+    const [unitsChecked, setUnitsChecked] = useState(true);
+    const [units, setUnits] = useState('imperial');
+
+    const { isLoading, data, error } = useFetchWeather(cityName, units);
 
     const classes = useStyles();
+
+    const handleUnitSwitch = () => {
+        setUnitsChecked(!unitsChecked);
+        setUnits(unitsChecked ? 'metric' : 'imperial');
+    };
 
     const handleSubmit = (event) => {
         setCityName(cityInputValue);
@@ -35,44 +47,77 @@ function App() {
                 alignItems="center"
                 className={classes.container}
             >
-                <Grid
-                    container
-                    direction="row"
-                    justify="center"
-                    alignItems="center"
-                >
-                    <Grid item>
-                        <form
-                            onSubmit={(event) => {
-                                handleSubmit(event);
-                            }}
+                <FormGroup>
+                    <form
+                        onSubmit={(event) => {
+                            handleSubmit(event);
+                        }}
+                    >
+                        <Grid
+                            container
+                            direction="row"
+                            justify="center"
+                            alignItems="center"
+                            spacing={2}
                         >
-                            <TextField
-                                id="city-search"
-                                className={classes.searchInput}
-                                label="City"
-                                variant="outlined"
-                                onChange={(event) =>
-                                    setCityInputValue(event.target.value)
-                                }
-                            />
-                            <IconButton
-                                type="submit"
-                                variant="contained"
-                                onClick={() => {
-                                    setCityName(cityInputValue);
-                                }}
-                            >
-                                <Icon className={classes.searchArrow}>
-                                    arrow_forward_ios
-                                </Icon>
-                            </IconButton>
-                        </form>
-                    </Grid>
-                </Grid>
+                            <Grid item>
+                                <TextField
+                                    id="city-search"
+                                    className={classes.searchInput}
+                                    label="City"
+                                    variant="outlined"
+                                    onChange={(event) =>
+                                        setCityInputValue(event.target.value)
+                                    }
+                                    InputProps={{
+                                        endAdornment: (
+                                            <IconButton
+                                                variant="contained"
+                                                onClick={() => {
+                                                    setCityName(cityInputValue);
+                                                }}
+                                            >
+                                                <Icon
+                                                    className={
+                                                        classes.SearchArrow
+                                                    }
+                                                >
+                                                    search
+                                                </Icon>
+                                            </IconButton>
+                                        ),
+                                    }}
+                                />
+                            </Grid>
+                            <Grid item>
+                                <Typography component="div">
+                                    <Grid
+                                        component="label"
+                                        container
+                                        alignItems="center"
+                                        spacing={1}
+                                    >
+                                        <Grid item>C</Grid>
+                                        <Grid item>
+                                            <UnitSwitch
+                                                checked={unitsChecked}
+                                                onChange={() =>
+                                                    handleUnitSwitch()
+                                                }
+                                                name="checked"
+                                            />
+                                        </Grid>
+                                        <Grid item>F</Grid>
+                                    </Grid>
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </FormGroup>
+                {console.log('data', data)}
                 {!isLoading && !error && (
                     <Paper className={classes.weatherForcast}>
-                        <h3>{cityName.toUpperCase()}</h3>
+                        <h3>{data.name}</h3>
                         <ul>
                             <li>Temp: {data.main.temp}</li>
                             <li>Feels Like: {data.main.feels_like}</li>
@@ -86,6 +131,40 @@ function App() {
         </Container>
     );
 }
+
+const UnitSwitch = withStyles((theme) => ({
+    root: {
+        width: 28,
+        height: 16,
+        padding: 0,
+        display: 'flex',
+    },
+    switchBase: {
+        padding: 2,
+        color: '#333333',
+        '&$checked': {
+            transform: 'translateX(12px)',
+            color: theme.palette.common.white,
+            '& + $track': {
+                opacity: 1,
+                backgroundColor: theme.palette.primary.main,
+                borderColor: theme.palette.primary.main,
+            },
+        },
+    },
+    thumb: {
+        width: 12,
+        height: 12,
+        boxShadow: 'none',
+    },
+    track: {
+        border: `1px solid ${theme.palette.grey[500]}`,
+        borderRadius: 16 / 2,
+        opacity: 1,
+        backgroundColor: theme.palette.common.white,
+    },
+    checked: {},
+}))(Switch);
 
 const useStyles = makeStyles({
     container: {

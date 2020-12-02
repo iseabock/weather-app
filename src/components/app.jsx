087a94bf6e751a55c5c275254259554ef02ac966
Ian Seabock <ianseabock@gmail.com>
2020-12-01 17:53:31 -0800

    Added router to support sharing city weather

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,55 +1,23 @@
-import React, { useState } from 'react';
-import { Container, Grid, fade, makeStyles } from '@material-ui/core';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import SearchWeather from './SearchWeather';
-import CurrentWeather from './CurrentWeather';
-import ForcastWeather from './ForcastWeather';
+import Weather from './Weather';
 
 import '../App.css';
 
 function App() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState('');
-    const [data, setData] = useState();
-
-    const classes = useStyles();
-
-    const handleDataChange = (props) => {
-        setIsLoading(props.isLoading);
-        setError(props.error);
-        setData(props.data);
-    };
-
     return (
-        <Container maxWidth="sm">
-            <h1>Weather</h1>
-            <Grid
-                container
-                direction="column"
-                justify="center"
-                alignItems="center"
-                className={classes.container}
-            >
-                <SearchWeather dataChange={handleDataChange} />
-                {!isLoading && !error && (
-                    <>
-                        <CurrentWeather data={data.weather} />
-                        <ForcastWeather data={data.forcast} />
-                    </>
-                )}
-            </Grid>
-        </Container>
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Weather />
+                </Route>
+                <Route path="/:cityName">
+                    <Weather />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
-
-const useStyles = makeStyles({
-    container: {
-        border: '1px solid #333',
-        borderRadius: 5,
-        padding: 20,
-        marginTop: 30,
-        background: fade('#FFFFFF', 0.8),
-    },
-});
 
 export default App;
